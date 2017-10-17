nasty007/android_build

commit 2a373c908ef0c40cdc9516e18035a0d4c644cd5a

One branch to rule them all!
This is simple. What we're doing here is grep-ing the manifest for
o8x-caf (our caf branch) and telling it to use X roomservice and if not
use the other one.

This keeps us from having to use two branches in the build repo, especially
since we're now using the vendor/extras.

It could be prettier for sure (I'm not a script wiz) but this works :-)

*Tested with bullhead and oneplus3*

Change-Id: I4bea9bbc087e26273e34a216d05d879ef665bcaa

envsetup.sh
@@ -565,26 +565,22 @@ function breakfast()
     DU_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+    if [[ $( grep -i "codeaurora" "${1}"manifest/o8x_default.xml) ]]; then
+    for caf in `/bin/ls vendor/du/caf-vendorsetup.sh 2> /dev/null`
         do
-            echo "including $f"
-            . $f
+            echo "including $caf"
+            . $caf
         done
-    unset f
-
-    if [ $# -eq 0 ]; then
-        # No arguments, so let's have the full menu
-        lunch
+    unset caf
     else
-        echo "z$target" | grep -q "-"
-        if [ $? -eq 0 ]; then
-            # A buildtype was specified, assume a full device name
-            lunch $target
-        else
-            # This is probably just the du model name
-            lunch du_$target-userdebug
-        fi
+    for aosp in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $aosp"
+            . $aosp
+        done
+    unset aosp
     fi
+
     return $?
 }
 
@@ -655,14 +651,13 @@ function lunch()
         # if we can't find the product, try to grab it from our github
         T=$(gettop)
         pushd $T > /dev/null
-        build/extras/tools/roomservice.py $product
+    if [[ $( grep -i "codeaurora" "${1}"manifest/o8x_default.xml) ]]; then
+        vendor/extras/tools/roomservice-caf.py $product
+    else
+        vendor/extras/tools/roomservice.py $product
+    fi
         popd > /dev/null
         check_product $product
-    else
-        T=$(gettop)
-        pushd $T > /dev/null
-        build/extras/tools/roomservice.py $product true
-        popd > /dev/null
     fi
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
@@ -1670,7 +1665,7 @@ function set_java_home() {
 function repopick() {
     set_stuff_for_environment
     T=$(gettop)
-    $T/build/extras/tools/repopick.py $@
+    $T/vendor/extras/tools/repopick.py $@
 }
 
 # Print colored exit condition
@@ -1773,16 +1768,25 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
     esac
 fi
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
-do
-    echo "including $f"
-    . $f
-done
-unset f
+# Execute the contents of any caf-vendorsetup.sh files we can find.
+if [[ $( grep -r "codeaurora" "${1}"manifest/* ) ]]; then
+    for caf in `test -d device && find -L device -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort` \
+             `test -d vendor && find -L vendor -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort` \
+             `test -d product && find -L product -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort`
+    do
+        echo "including $caf"
+        . $caf
+    done
+        unset caf
+        else
+    for aosp in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
+             `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
+             `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
+    do
+        echo "including $aosp"
+        . $aosp
+    done
+        unset aosp
+fi
 
 addcompletions
-
-export ANDROID_BUILD_TOP=$(gettop)

core/Makefile
@@ -2764,7 +2764,7 @@ $(INTERNAL_FINDBUGS_HTML_TARGET): $(INTERNAL_FINDBUGS_XML_TARGET)
 # These are some additional build tasks that need to be run.
 ifneq ($(dont_bother),true)
 include $(sort $(wildcard $(BUILD_SYSTEM)/tasks/*.mk))
-include $(sort $(wildcard $(TOPDIR)build/extras/tasks/*.mk))
+include $(sort $(wildcard $(TOPDIR)vendor/extras/tasks/*.mk))
 -include $(sort $(wildcard vendor/*/build/tasks/*.mk))
 -include $(sort $(wildcard device/*/build/tasks/*.mk))
 -include $(sort $(wildcard product/*/build/tasks/*.mk))

core/config.mk
@@ -310,7 +310,7 @@ include $(BUILD_SYSTEM)/combo/select.mk
 endif
 
 ifndef KATI
-include $(TOPDIR)build/extras/core/ccache.mk
+include $(TOPDIR)vendor/extras/core/ccache.mk
 include $(BUILD_SYSTEM)/goma.mk
 
 export CC_WRAPPER
@@ -889,5 +889,8 @@ include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
 include $(TOPDIR)vendor/extras/core/dumpvar.mk
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(TOPDIR)vendor/extras/core/pathmap.mk
 include $(TOPDIR)vendor/extras/core/qcom_target.mk
+endif

commit 7438ff0bbc1783d721fe689cf3f92df78900284a

Build: Fix breakfast
Breakfast is a special pass through to lunch, which was accidentally
removed in the merging of the AOSP and CAF branches. Add it back!

Change-Id: Ie185830605993d784b14c60fd673afcd9735acc7
Fixes: 2a373c908ef0 ("One branch to rule them all!")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

envsetup.sh
@@ -581,6 +581,20 @@ function breakfast()
         unset aosp
     fi
 
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the du model name
+            lunch du_$target-userdebug
+        fi
+    fi
+
     return $?
 }
 